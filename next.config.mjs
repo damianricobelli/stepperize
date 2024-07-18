24f4ece263ac0da085cc3233a97401b57d63683f
Ricardo Castro <soyricardodev@proton.me>
2024-07-18 02:16:30 -0400

    feat: add ignore typescript and eslint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+};
 
 export default nextConfig;
