1e0451ca4a1e45dea76a2f0e833bf3f9fa98d85c
Ricardo Castro <soyricardodev@proton.me>
2024-07-18 01:57:34 -0400

    fix: solved 3/4 biome errors

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -95,9 +95,9 @@ export const reducer = (state: State, action: Action): State => {
 			if (toastId) {
 				addToRemoveQueue(toastId);
 			} else {
-				state.toasts.forEach((toast) => {
+				for (const toast of state.toasts) {
 					addToRemoveQueue(toast.id);
-				});
+				}
 			}
 
 			return {
@@ -132,9 +132,9 @@ let memoryState: State = { toasts: [] };
 
 function dispatch(action: Action) {
 	memoryState = reducer(memoryState, action);
-	listeners.forEach((listener) => {
+	for (const listener of listeners) {
 		listener(memoryState);
-	});
+	}
 }
 
 type Toast = Omit<ToasterToast, "id">;
@@ -156,7 +156,9 @@ function toast({ ...props }: Toast) {
 			id,
 			open: true,
 			onOpenChange: (open) => {
-				if (!open) dismiss();
+				if (!open) {
+					dismiss();
+				}
 			},
 		},
 	});
@@ -171,6 +173,7 @@ function toast({ ...props }: Toast) {
 function useToast() {
 	const [state, setState] = React.useState<State>(memoryState);
 
+	// biome-ignore lint/correctness/useExhaustiveDependencies: Maybe this "state" is necessary because is updated by the listener, need to be checked
 	React.useEffect(() => {
 		listeners.push(setState);
 		return () => {
