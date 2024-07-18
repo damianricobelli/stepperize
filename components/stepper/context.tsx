6baa4b09290fb78e697ac0aa5fcca88ce6311a48
Ricardo Castro <soyricardodev@proton.me>
2024-07-18 01:54:12 -0400

    style: biome format

--- components/stepper/context.tsx
+++ components/stepper/context.tsx
@@ -1,78 +1,77 @@
-import * as React from "react"
-import type { StepperProps } from "./types"
+import * as React from "react";
+import type { StepperProps } from "./types";
 
 interface StepperContextValue extends StepperProps {
-  clickable?: boolean
-  isError?: boolean
-  isLoading?: boolean
-  isVertical?: boolean
-  stepCount?: number
-  expandVerticalSteps?: boolean
-  activeStep: number
-  initialStep: number
+	clickable?: boolean;
+	isError?: boolean;
+	isLoading?: boolean;
+	isVertical?: boolean;
+	stepCount?: number;
+	expandVerticalSteps?: boolean;
+	activeStep: number;
+	initialStep: number;
 }
 
 type StepperContextProviderProps = {
-  value: Omit<StepperContextValue, "activeStep">
-  children: React.ReactNode
-}
+	value: Omit<StepperContextValue, "activeStep">;
+	children: React.ReactNode;
+};
 
 const StepperContext = React.createContext<
-  StepperContextValue & {
-    nextStep: () => void
-    prevStep: () => void
-    resetSteps: () => void
-    setStep: (step: number) => void
-  }
+	StepperContextValue & {
+		nextStep: () => void;
+		prevStep: () => void;
+		resetSteps: () => void;
+		setStep: (step: number) => void;
+	}
 >({
-  steps: [],
-  activeStep: 0,
-  initialStep: 0,
-  nextStep: () => {},
-  prevStep: () => {},
-  resetSteps: () => {},
-  setStep: () => {},
-})
-
+	steps: [],
+	activeStep: 0,
+	initialStep: 0,
+	nextStep: () => {},
+	prevStep: () => {},
+	resetSteps: () => {},
+	setStep: () => {},
+});
 
 const StepperProvider = ({ value, children }: StepperContextProviderProps) => {
-  const isError = value.state === "error"
-  const isLoading = value.state === "loading"
+	const isError = value.state === "error";
+	const isLoading = value.state === "loading";
 
-  const [activeStep, setActiveStep] = React.useState(value.initialStep)
+	const [activeStep, setActiveStep] = React.useState(value.initialStep);
 
-  const nextStep = () => {
-    setActiveStep((prev) => prev + 1)
-  }
+	const nextStep = () => {
+		setActiveStep((prev) => prev + 1);
+	};
 
-  const prevStep = () => {
-    setActiveStep((prev) => prev - 1)
-  }
+	const prevStep = () => {
+		setActiveStep((prev) => prev - 1);
+	};
 
-  const resetSteps = () => {
-    setActiveStep(value.initialStep)
-  }
+	const resetSteps = () => {
+		setActiveStep(value.initialStep);
+	};
 
-  const setStep = (step: number) => {
-    setActiveStep(step)
-  }
+	const setStep = (step: number) => {
+		setActiveStep(step);
+	};
 
-  return (
-    <StepperContext.Provider
-      value={{
-        ...value,
-        isError,
-        isLoading,
-        activeStep,
-        nextStep,
-        prevStep,
-        resetSteps,
-        setStep,
-      }}
-    >
-      {children}
-    </StepperContext.Provider>
-  )
-}
+	return (
+		<StepperContext.Provider
+			value={{
+				...value,
+				isError,
+				isLoading,
+				activeStep,
+				nextStep,
+				prevStep,
+				resetSteps,
+				setStep,
+			}}
+		>
+			{children}
+		</StepperContext.Provider>
+	);
+};
 
-export { StepperContext, StepperProvider }
+export { StepperContext, StepperProvider };
--- components/stepper/step-button-container.tsx
+++ components/stepper/step-button-container.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import type * as React from "react";
 
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
