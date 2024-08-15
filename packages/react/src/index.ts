17102b3bc1a31454efc3e1a2601e2a747da40616
Damian Ricobelli <dricobelli@gmail.com>
2024-08-15 19:29:58 -0300

    packages: add comments

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,13 +1,17 @@
+// Types
 export type { StepperProviderProps, Step } from "./stepper";
 export type { UseStepActions } from "./context";
 export type { ExtendedStep } from "./hooks/use-stepper";
 
+// Components
 export { Stepper } from "./stepper";
 export { StepperStep } from "./stepper-step";
 export { StepperProviderContext, StepContext } from "./context";
 
+// Helpers
 export { createSteps } from "./helpers";
 
+// Hooks
 export { useStepper } from "./hooks/use-stepper";
 export { useStepperConfig } from "./hooks/use-stepper-config";
 export { useStepperResponsive } from "./hooks/use-stepper-responsive";
