03e7fc7104479ec831c3edf5b3779041619fc45c
Damian Ricobelli <dricobelli@gmail.com>
2024-08-19 17:59:19 -0300

    docs: improve the docs with scoped examples

--- docs/examples/scoped-stepper.tsx
+++ docs/examples/scoped-stepper.tsx
@@ -1,26 +1,26 @@
-import * as React from 'react'
-
-import { Button } from "@/components/ui/button"
-import { defineStepper } from "@stepperize/react"
+import { Button } from "@/components/ui/button";
+import { defineStepper } from "@stepperize/react";
 
 const { useStepper, Scoped } = defineStepper(
 	{ id: "first", title: "First" },
 	{ id: "second", title: "Second" },
 	{ id: "third", title: "Third" },
 	{ id: "last", title: "Last" },
-)
+);
 
 export const MyScopedStepper = () => {
 	return (
-		<Scoped>
-			<MySteps />
-			<MyActions />
-		</Scoped>
-	)
-}
+		<div className="flex flex-col gap-4 bg-gray-3 p-4 my-4 rounded-md">
+			<Scoped>
+				<MySteps />
+				<MyActions />
+			</Scoped>
+		</div>
+	);
+};
 
 const MySteps = () => {
-	const stepper = useStepper()
+	const stepper = useStepper();
 
 	return (
 		<>
@@ -40,11 +40,11 @@ const MySteps = () => {
 				<p>This is the {step.id} step. So it ends.</p>
 			))}
 		</>
-	)
-}
+	);
+};
 
 const MyActions = () => {
-	const stepper = useStepper()
+	const stepper = useStepper();
 
 	return !stepper.isLastStep ? (
 		<div className="flex items-center gap-2">
@@ -53,12 +53,16 @@ const MyActions = () => {
 			</Button>
 
 			<Button onClick={stepper.goToNextStep}>
-				{stepper.when("third", () => "Finish", () => "Next")}
+				{stepper.when(
+					"third",
+					() => "Finish",
+					() => "Next",
+				)}
 			</Button>
 		</div>
 	) : (
 		<div className="flex items-center gap-2">
 			<Button onClick={stepper.reset}>Reset</Button>
 		</div>
-	)
-}
+	);
+};
