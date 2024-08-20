ef8d71ec165b799f1626d110b0508f816753eb49
Damian Ricobelli <dricobelli@gmail.com>
2024-08-19 22:31:32 -0300

    docs: formatting library

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -1,4 +1,4 @@
-export type Step = { id: string } & Record<string, any>
+export type Step = { id: string } & Record<string, any>;
 
 export type Stepper<Steps extends Step[] = Step[]> = {
 	steps: Steps;
@@ -14,17 +14,17 @@ export type Stepper<Steps extends Step[] = Step[]> = {
 	when: <Id extends Get.Id<Steps>, R1, R2>(
 		id: Id,
 		whenFn: (step: Get.StepById<Steps, Id>) => R1,
-		elseFn?: (step: Get.StepSansId<Steps, Id>) => R2
+		elseFn?: (step: Get.StepSansId<Steps, Id>) => R2,
 	) => R1 | R2;
 };
 
 export namespace Get {
 	/** Returns a union of possible IDs from the given Steps. */
-	export type Id<Steps extends Step[] = Step[]> = Steps[number]['id']
+	export type Id<Steps extends Step[] = Step[]> = Steps[number]["id"];
 
 	/** Returns a Step from the given Steps with the given Step Id. */
-	export type StepById<Steps extends Step[], Id extends Get.Id<Steps>> = Extract<Steps[number], { id: Id }>
+	export type StepById<Steps extends Step[], Id extends Get.Id<Steps>> = Extract<Steps[number], { id: Id }>;
 
 	/** Returns any Steps from the given Steps without the given Step Id. */
-	export type StepSansId<Steps extends Step[], Id extends Get.Id<Steps>> = Exclude<Steps[number], { id: Id }>
+	export type StepSansId<Steps extends Step[], Id extends Get.Id<Steps>> = Exclude<Steps[number], { id: Id }>;
 }
