429fae021a07258b70afc6da4cb51436d4acc38e
Alex <alexzhang1030@foxmail.com>
2025-01-06 22:36:08 +0800

    chore: lint

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
-  dts: true,
-  sourcemap: false,
-  clean: true,
-  minify: true,
-  treeshake: true,
-  tsconfig: "tsconfig.json",
+	entry: ["src/index.ts"],
+	format: ["cjs", "esm"],
+	dts: true,
+	sourcemap: false,
+	clean: true,
+	minify: true,
+	treeshake: true,
+	tsconfig: "tsconfig.json",
 });
