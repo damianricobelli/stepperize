6af1e237f2bfa4e5d2b96368fb55ac31bd67d520
Damian Ricobelli <dricobelli@gmail.com>
2025-01-25 16:29:10 -0300

    docs: add new function docs

--- apps/docs/components/hero.tsx
+++ apps/docs/components/hero.tsx
@@ -4,37 +4,66 @@ import { Button } from "@/components/ui/button";
 import { defineStepper } from "@stepperize/react";
 
 const { useStepper } = defineStepper(
-	{ id: "first", title: "Step 1", description: "This is the content for step 1." },
-	{ id: "second", title: "Step 2", description: "This is the content for step 2." },
-	{ id: "third", title: "Step 3", description: "This is the content for step 3." },
-	{ id: "last", title: "Step 4", description: "This is the content for step 4." },
+  {
+    id: "first",
+    title: "Step 1",
+    description: "This is the content for step 1.",
+  },
+  {
+    id: "second",
+    title: "Step 2",
+    description: "This is the content for step 2.",
+  },
+  {
+    id: "third",
+    title: "Step 3",
+    description: "This is the content for step 3.",
+  },
+  {
+    id: "last",
+    title: "Step 4",
+    description: "This is the content for step 4.",
+  }
 );
 
 export function HeroStepper() {
-	const stepper = useStepper();
-	return (
-		<div className="p-6 shadow-lg border rounded-lg">
-			<h3 className="text-lg font-semibold mb-2">{stepper.current.title}</h3>
-			<p className="text-muted-foreground mb-4">{stepper.current.description}</p>
-			<div className="flex justify-between">
-				{!stepper.isLast ? (
-					<>
-						<Button onClick={stepper.prev} disabled={stepper.isFirst}>
-							Previous
-						</Button>
+  const stepper = useStepper();
+  return (
+    <div className="p-6 shadow-lg border rounded-lg">
+      <h3 className="text-lg font-semibold mb-2">{stepper.current.title}</h3>
+      <p className="text-muted-foreground mb-4">
+        {stepper.current.description}
+      </p>
+      <div className="flex justify-between">
+        {!stepper.isLast ? (
+          <>
+            <Button
+              onClick={() => {
+                stepper.beforePrev(() => {
+                  return new Promise((resolve) => {
+                    setTimeout(() => {
+                      resolve(true);
+                    }, 1000);
+                  });
+                });
+              }}
+              disabled={stepper.isFirst}
+            >
+              Previous
+            </Button>
 
-						<Button onClick={stepper.next}>
-							{stepper.when(
-								"third",
-								() => "Finish",
-								() => "Next",
-							)}
-						</Button>
-					</>
-				) : (
-					<Button onClick={stepper.reset}>Reset</Button>
-				)}
-			</div>
-		</div>
-	);
+            <Button onClick={stepper.next}>
+              {stepper.when(
+                "third",
+                () => "Finish",
+                () => "Next"
+              )}
+            </Button>
+          </>
+        ) : (
+          <Button onClick={stepper.reset}>Reset</Button>
+        )}
+      </div>
+    </div>
+  );
 }
